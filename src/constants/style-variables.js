cc36ec8696525c0b4c2c7c992096abdf7f85085e
amelnikov1 <amelnikov@zaraffasoft.com>
2019-09-10 17:03:04 +0300

    created style variables

---
+++ src/constants/style-variables.js
@@ -0,0 +1,26 @@
+const colors = {
+    white: '#fff',
+    black: '#000'
+};
+
+const content = {
+    large: 15,
+    medium: 30,
+    medium_x: 40,
+    small: 50,
+    tiny: 80
+};
+
+const fontSizes = {
+    h1: 38,
+    h2: 28,
+    h3: 20,
+    body: 16,
+    caption: 12
+};
+
+export default {
+    content,
+    fontSizes,
+    colors
+};
