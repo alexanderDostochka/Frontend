5f0b7ad511336b69c5378292bacabacffe824a5a
amelnikov1 <amelnikov@zaraffasoft.com>
2019-09-10 17:03:12 +0300

    created main screen

---
+++ src/screens/Main/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'react-native';
+import Content from '../../components/Content';
+
+const Main = () => {
+    return (
+        <Content type="large">
+            <Text>Hello React</Text>
+        </Content>
+    );
+};
+
+export default Main;
