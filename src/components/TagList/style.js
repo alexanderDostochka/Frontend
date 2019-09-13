a3695d2d0e8aa70c9424f82be6763c2cb9e64357
amelnikov1 <amelnikov@zaraffasoft.com>
2019-09-14 01:37:51 +0300

    Created style for TagList

---
+++ src/components/TagList/style.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+import variables from '../../constants/style-variables';
+
+export default StyleSheet.create({
+    list: {
+        flexDirection: 'row',
+        paddingVertical: 15,
+        overflow: 'hidden'
+    }
+});
