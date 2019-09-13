3974362b7bb2624af22057d8980882495891e727
amelnikov1 <amelnikov@zaraffasoft.com>
2019-09-14 01:35:39 +0300

    restyle header

--- src/navigation.js
+++ src/navigation.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack'
 import { Image } from 'react-native';
+import variables from './constants/style-variables';
 
 // screens
 import Main from './screens/Main';
@@ -10,11 +11,23 @@ const navigation = createStackNavigator({
     Main
 }, {
     defaultNavigationOptions: {
-        headerStyle: {},
+        headerStyle: {
+            height: 60,
+            elevation: 0,
+            borderBottomWidth: 0.3,
+            borderColor: variables.colors.silver
+        },
         headerBackImage: <Image />,
         headerBackTitle: null,
-        headerLeftContainerStyle: {},
-        headerRightContainerStyle: {},
+        headerLeftContainerStyle: {
+            paddingLeft: variables.content.large
+        },
+        headerRightContainerStyle: {
+            justifyContent: 'flex-end',
+            width: '100%',
+            paddingLeft: variables.content.large,
+            paddingRight: variables.content.large
+        },
     }
 });
 
