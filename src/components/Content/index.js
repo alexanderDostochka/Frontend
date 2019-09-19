cb04a50eb49e5a7b797f0c4776409750db85e0cc
amelnikov1 <amelnikov@zaraffasoft.com>
2019-09-19 18:28:01 +0300

    added margin

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -4,9 +4,15 @@ import { View } from 'react-native';
 import variables from '../../constants/style-variables';
 
 const Content = props => {
-    const { children, type } = props;
+    const { children, type, margin } = props;
     
     const contentStyle = [
+        {
+            marginTop: margin[0],
+            marginBottom: margin[1],
+            marginLeft: margin[2],
+            marginRight: margin[3]
+        },
         {
             paddingLeft: variables.content[type],
             paddingRight: variables.content[type],
@@ -21,11 +27,13 @@ const Content = props => {
 }
 
 Content.propTypes = {
-    type: PropTypes.string
+    type: PropTypes.string,
+    margin: PropTypes.array
 }
 
 Content.defaultProps = {
-    type: "large"
+    type: "",
+    margin: []
 }
 
 export default Content;
