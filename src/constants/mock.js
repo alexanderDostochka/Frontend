2d1052f584d80dea35bf14a99c794f94e902324f
Alexander Melnikov <amelnikov@zaraffasoft.com>
2019-09-23 00:07:22 +0300

    added categories to mock

--- src/constants/mock.js
+++ src/constants/mock.js
@@ -1,3 +1,22 @@
 export const lang = {
     search: 'Search...'
 };
+
+export const categories = [
+    {
+        type: 'react',
+        title: 'ReactJS'
+    },
+    {
+        type: 'angular',
+        title: 'Angular'
+    },
+    {
+        type: 'vue',
+        title: 'Vue'
+    },
+    {
+        type: 'js',
+        title: 'JS'
+    }
+];
