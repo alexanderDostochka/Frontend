3eedb036c8506964eda543a4ae0b9ff06b01f102
Alexander Melnikov <amelnikov@zaraffasoft.com>
2019-10-22 02:11:26 +0300

    created config for redux

--- src/redux/config.js
+++ src/redux/config.js
@@ -1,8 +1,20 @@
-import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware  from 'redux-saga';
+import logger from 'redux-logger'
 import rootReducers from './reducers';
+import rootSagas from './sagas';
 
-const config = createStore(rootReducers, composeWithDevTools(applyMiddleware(thunk)));
+const configureStore = () => {
+    const sagaMiddleware = createSagaMiddleware();
 
-export default config;
+    const store = createStore(
+        rootReducers,
+        applyMiddleware(sagaMiddleware, logger)
+    );
+
+    sagaMiddleware.run(rootSagas);
+
+    return store;
+}
+
+export default configureStore;
