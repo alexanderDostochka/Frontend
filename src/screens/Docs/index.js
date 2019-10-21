42831f8c4abebe693a6b24a8048c81b4d28d2e40
Alexander Melnikov <amelnikov@zaraffasoft.com>
2019-10-22 02:14:49 +0300

    Remake docs screen

--- src/screens/Docs/index.js
+++ src/screens/Docs/index.js
@@ -1,31 +1,59 @@
-import React from 'react';
-import { StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { FlatList } from 'react-native';
 import Logo from '../../components/Logo';
-import Content from '../../components/Content';
-import Text from '../../components/Text';
+import { getDocs } from '../../redux/actions/docs';
+import ScreenLayout from '../../components/ScreenLayout';
+import Item from './Item';
+import _ from 'lodash';
 
-// TODO: Move to redux
-import { categories } from '../../constants/mock';
+const Docs = ({ navigation, getDocs, data, loader }) => {
+    const type = navigation.state.params.type;
 
-const Docs = ({ navigation }) => {
+    useEffect(() => { 
+        getDocs({type});
+    }, [0]);
+
     return (
-        <Content type="large">
-            <Text> Hi </Text>
-        </Content>
+        <ScreenLayout loader={ loader }>
+            <FlatList
+                data={ data }
+                renderItem={({ item }) => <Item item={ item } navigation={ navigation } />}
+                keyExtractor={item => item.id}
+            />
+        </ScreenLayout>
     );
 };
 
-const styles = StyleSheet.create({
+Docs.propTypes = {
+    navigation: PropTypes.object,
+    getDocs: PropTypes.func,
+    data: PropTypes.array,
+    loader: PropTypes.bool
+};
 
-});
+Docs.defaultProps = {
+    navigation: {},
+    getDocs: _.noop,
+    data: [],
+    loader: true
+};
 
 Docs.navigationOptions = ({ navigation })  => ({
     headerTitle: (
         <Logo 
             padding={ false } 
-            image={ navigation.state.params.type } 
+            image={ `${navigation.state.params.type}.png` } 
             title={ navigation.state.params.title } />
     )
 });
 
-export default Docs;
+const mapStateToProps = state => {
+    return {
+        data: state.docs.docs,
+        loader: state.docs.loader
+    }
+}
+
+export default connect(mapStateToProps, { getDocs })(Docs);
