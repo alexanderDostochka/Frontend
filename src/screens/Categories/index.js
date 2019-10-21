d8913987e70013ac83407b1b376334d1c9497eba
Alexander Melnikov <amelnikov@zaraffasoft.com>
2019-10-22 02:13:55 +0300

    Remake categories screen

--- src/screens/Categories/index.js
+++ src/screens/Categories/index.js
@@ -1,56 +1,73 @@
-import React from 'react';
-import { FlatList, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { FlatList, TouchableOpacity } from 'react-native';
 import Logo from '../../components/Logo';
+import Image from '../../components/Image';
+import ScreenLayout from '../../components/ScreenLayout';
 import Content from '../../components/Content';
-import Category from '../../components/Category';
 import Text from '../../components/Text';
+import { getCategories } from '../../redux/actions/categories';
+import { lang } from '../../constants/mock';
+import Item from './Item';
+import _ from 'lodash';
 
-// TODO: Move to redux
-import { categories } from '../../constants/mock';
-
-const Categories = ({ navigation }) => {
-    const gotoDoc = (type, title) => {
-        navigation.navigate('Docs', { type, title });
-    };
-
-    const renderItem = item => (
-        <Content margin={[0, 10, 0, 10]}>
-            <TouchableOpacity onPress={() => gotoDoc(item.type, item.title)} activeOpacity={0.8}>
-                <Category title={ item.title } type={ item.type } />
-            </TouchableOpacity>
-        </Content>
-    );
+const Categories = (props) => {
+    const { navigation, getCategories, categories, loader } = props;
+    const [sort, setSort] = useState(false);
+    
+    useEffect(() => {
+        getCategories();
+    }, []);
 
     return (
-        <Content type="large">
-            <Content margin={[25]}>
-                <Content margin={[0, 15]}>
-                    <Text size="h3" weight="300">Categories</Text>
+        <ScreenLayout loader={ loader }>
+            <Content type="large">
+                <Content margin={[25]}>
+                    <Content margin={[0, 15]} spaceEvenly>
+                        <Text size="h3" weight="300">{ lang.categories }</Text>
+                        <TouchableOpacity onPress={() => setSort(!sort)}>
+                            <Image style={{ width: 24, height: 24 }} path={`sort_${sort ? 't' : 'c'}.png`} />
+                        </TouchableOpacity>
+                    </Content>
+                        <FlatList
+                            data={ categories }
+                            renderItem={({ item }) => 
+                                <Item item={ item } navigation={ navigation } sort={ sort }/>
+                            }
+                            keyExtractor={item => item.type}
+                            numColumns={sort ? 1 : 3}
+                            key={sort ? 1 : 3}
+                        />
                 </Content>
-                <FlatList
-                    contentContainerStyle={ styles.list }
-                    style={ styles.categories }
-                    data={ categories }
-                    renderItem={({ item }) => renderItem(item)}
-                    keyExtractor={item => item.type}
-                    numColumns={3}
-                />
             </Content>
-        </Content>
+        </ScreenLayout>
     );
 };
-
-const styles = StyleSheet.create({
-    list: {
-       
-    
-    }
-});
 
 Categories.navigationOptions  = {
     headerTitle: (
-        <Logo image="js" title="Docs" />
+        <Logo image="js.png" title={ lang.docs } />
     )
 };
 
-export default Categories;
+Categories.propTypes = {
+    getCategories: PropTypes.func,
+    categories: PropTypes.array,
+    loader: PropTypes.bool
+}
+
+Categories.defaultProps = {
+    getCategories: _.noop,
+    categories: [],
+    loader: true
+}
+
+const mapStateToProps = state => {
+    return {
+        categories: state.categories.categories,
+        loader: state.categories.loader
+    }
+}
+
+export default connect(mapStateToProps, { getCategories })(Categories);
