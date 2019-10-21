6e054fd0ac9525d56cbc7536831b42c05b6c9367
Alexander Melnikov <amelnikov@zaraffasoft.com>
2019-10-22 02:07:14 +0300

    added setBackgroundColor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
+import { StatusBar } from 'react-native';
 import reduxConfig from './redux/config';
 import Navigation from './navigation';
 
 const App = () => {
+    useEffect(() => {
+        StatusBar.setBackgroundColor('white');
+        StatusBar.setBarStyle('dark-content');
+    }, [0]);
+
     return (
-        <Provider store={ reduxConfig }>
+        <Provider store={ reduxConfig() }>
             <Navigation />
         </Provider>
     );
